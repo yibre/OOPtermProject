bb0d593ce2fcaecb25fb60159af6cdb23a4e50bd
yibre <cdoyue@gmail.com>
2021-11-02 13:59:01 +0900

    class basic structure building now

--- Classes.h
+++ Classes.h
@@ -3,28 +3,36 @@
 
 using namespace std;
 
-struct Bilingual {
-    bool flag; // true면 선택가능, false면 그대로 1번 보냄
-    const string* mentions[10][2] = {
-        {"반갑습니다", "Hello sir"},
-        {"테스트", "test"}
-    };
-
+const char* mentions[10][2] = {
+    {"반갑습니다", "Hello sir"},
+    {"테스트", "test"},
+    {"카드를 넣어주세요", "gensin impact"}
 };
+
+class Account;
+class ATM;
+class User;
+class Bank;
 
 class History {
 private:
     // 2d  array;
 public:
-    History();
-    ~History();
-    vector<vector<string>> History;
+    History() {};
+    ~History() {};
+    // atm history 보관소
+    vector<vector<string> > atmhis; //
+    // user가 세션 종료 후 받게 되는 명세서
+    vector<vector<string> > userhis;
+    // 1개 단위 거래 종료 후 받게 되는 transaction history
+    vector<vector<string> > transactionhis;
+
     //char History[][10];
     // history 를 보관하는 2d 어레이
     /*
     번호 username userid  거래타입(입출송) 거래계좌id 상대계좌id 거래액수 거래전잔액 거래후잔액 날짜
     ----------------------
-    1 김수한무     3        송금
+    1 김수한무     3        송금         1         3
     ----------------------
     2
     -----------------------
@@ -41,41 +49,51 @@ public:
     User() {};
     ~User() {};
     // account list가 들어가는 범위 공개
-    Account* accountlist[]; // test를 위하여 설정해둠
+    Account* accountlist[300]; // test를 위하여 설정해둠
 };
 
 class Bank {
 private:
     char ID; // bank는 B1, B2, B3 이렇게 시작
+    static int addID; // 0부터 시작해서 1씩 증가
     string name;
 
 public:
+    Bank() {};
+    ~Bank() {};
     Account* accountlist[300];
-
 };
 
 class ATM {
 private:
     char ID; // atm은 A1 A2 A3 이렇게 시작
+    static int addID; // id 부여용 넘버, 0부터 시작해서 1씩 증가
     Bank *ownerBank;
     char adminID;
     int adminpw;
+    int cash; // atm 내부 현금 총액
+    int check; // atm 내부 수표 총액
+    bool bilingual;
+    History* atmhis;
 
 public:
     static History* getInstance();
     bool checkID(char);
     bool checkpw(int);
-
+    void deposit(char, int); // 입금함수, 입금액
+    void withdrawal(int); // 출금함수, 출금액
 };
 
 
 class Account {
 private:
     int ID; // ID는 0부터 시작해 1씩 늘려감, 순수 숫자
-    static int addID = 0; // 어떤 ID를 부여할것인가
+    static int addID; // 어떤 ID를 부여할것인가
     Bank *ownerBank;
     User *owner;
     int passward; // password는 순수 integer 구성, 카드와 동일
+    int balance; // 잔액
+    History* accounthis;
 
 public:
     Account() {};
@@ -83,5 +101,8 @@ public:
     int getID();
     bool checkPassward(int);
     // int 타입의 패스워드를 받아 해당 코드가 맞는지 확인
-    
+    void deposit(char, int); // 입금, 입금액 타입(캐시, 수표) 입금액 인풋,
+    void remittance(int, int); //송금 계좌번호, 액수 
+    void widthrawal(int); // 출금
 };
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
- #include "Classes.h"
+#include "Classes.h"
 #include <iostream>
 #include <string>
+
+/*
+0: 세션 대기, 프로그램 시작하고 난 직후 이후 다시 0 될 일 없음
+1: 세션 진행중. 프로그램 시작 후 1로 넘어감
+admin이 atm 기록 볼 때도 status 1임
+2: 계좌번호 확인 과정
+3: 거래종류 선택 중 / 입금, 출금, 송금
+4: 입금
+5: 출금
+6: 송금
+7: 한 세션 종료 flag, 세션 결과 출력 후 다시 1로 넘어감
+*/
+#define UserStatus 0
+
 
 using namespace std;
 
@@ -8,8 +22,11 @@ int main() {
 
     string str;
     while (cin >> str) {
+
+        // exit 을 타입할 시 단계와 무관하게 세션 종료
+        // 프로그램 종료에 대한 input 기준은 없음
         if (str == "exit") {break;}
-        if (cin.eof() == true) {break;}
+        // if (cin.eof() == true) {break;}
         cout << str << endl;
 
     }
