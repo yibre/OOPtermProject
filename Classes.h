5cae9192b48be6dae89a19e39249f697d5668f75
shyapple1203 <shyapple1203@naver.com>
2021-11-09 14:37:20 +0900

    deposit, withdrawal
    deposit, withdrawal 기본 기능 구현

--- Classes.h
+++ Classes.h
@@ -27,12 +27,41 @@ private:
 	string ID; // bank는 B1, B2, B3 이렇게 시작
 	static int addID; // 0부터 시작해서 1씩 증가
 	string name;
+	Account* accountlist[100];
 
 public:
-	Bank() { ID = "B1"; name = "hellothere"; }
+	Bank() { ID = "B1"; name = "uriBank"; }
 	Bank(string name) { this->name = name; }
-	~Bank() {};
+	~Bank() { };
 	string getBankName() { return name; }
+};
+
+class Account {
+private:
+	int ID; // ID는 0부터 시작해 1씩 늘려감, 순수 숫자
+	static int numID; // 어떤 ID를 부여할것인가
+	Bank* ownerBank;
+	User* owner;
+	int password; // password는 순수 integer 구성, 카드와 동일
+	int balance; // 잔액
+
+public:
+
+	Account() { numID = 0; }
+	Account(Bank* bank, User* owner, int pw, int balance);
+	~Account();
+	int getID() { return ID; }
+	bool checkPassward(int); // int 타입의 패스워드를 받아 해당 패스워드가 맞는지 확인
+	void deposit(int type, int money) { // 입금, 입금액 타입(캐시, 수표) 입금액 인풋,
+		this->balance += money;
+	};
+	void remittance(int, int); //송금 계좌번호, 액수 
+	void withdrawal(int money) { // 출금
+		this->balance -= money;
+	};
+	void increaseID() { numID++; }
+	int getNumID() { return numID; }
+	int getBalance() { return balance; }
 };
 
 class ATM {
@@ -44,6 +73,10 @@ private:
 	int adminpw;
 	int remainCash; // atm 내부 현금 총액
 	int remainCheck = 0; // atm 내부 수표 총액
+	int remainCheckNum = 0; // atm 내부 수표 갯수
+	int maxWithdrawal = 300000;
+	int fee = 500;
+	int maxCashNumber = 100;
 	bool engSupport;
 
 public:
@@ -52,36 +85,46 @@ public:
 	~ATM();
 	bool checkID(char);
 	bool checkpw(int);
-	void deposit(char, int); // 입금함수, 입금액
-	void withdrawal(int); // 출금함수, 출금액
+	bool deposit(int type, int money, int paperNum, Account* acc) { // 입금함수, 입금액 (type1 : 현금 type2 : 수표)
+		if (type == 1 && paperNum > 50) {
+			cout << "최대 입금 가능 장 수를 초과하였습니다. 다시 시도해주십시오." << endl; // 입금 가능 최대 장 수를 제한해야하는데 어떻게 구현해야할까?
+			return false;
+		}
+		if (type == 2 && paperNum > 30) {
+			cout << "최대 입금 가능 장 수를 초과하였습니다. 다시 시도해주십시오." << endl; // 입금 가능 최대 장 수를 제한해야하는데 어떻게 구현해야할까?
+			return false;
+		}
+		acc->deposit(type, money);
+		if (type == 1) { this->remainCash += money; }
+		else { this->remainCheck += money; this->remainCheckNum += paperNum; }
+		cout << money << "원이 입금되었습니다." << endl;
+		cout << "잔액 : " << acc->getBalance() << " 원" << endl;
+		return true;
+	}; 
+	bool withdrawal(int money, Account* acc) { // 출금함수, 출금액
+		if ((money + fee) > acc->getBalance()) {
+			cout << "계좌에 잔액이 부족합니다. 다시 시도해주십시오." << endl;
+			return false;
+		}
+		if (money > this->remainCash) {
+			cout << "ATM 기기에 현금이 부족합니다. 다시 시도해주십시오." << endl;
+			return false;
+		}
+		if (money > this->maxWithdrawal) {
+			cout << "1회 최대 출금 금액(30만원)을 초과하였습니다. 다시 시도해주십시오." << endl;
+			return false;
+		}
+		acc->withdrawal(money + fee);
+		this->remainCash -= money;
+		cout << money << "원이 출금되었습니다. 투입구를 확인해주십시오." << endl;
+		cout << "수수료 : "<< fee << " 원";
+		cout << "잔액 : " << acc->getBalance() << " 원" << endl;
+		return true;
+	};
 	void IncreaseID() { numID++; }
 	int getNumID() { return numID; }
 	int getATMremainCash() { return remainCash; }
-};
-
-
-class Account {
-private:
-	int ID; // ID는 0부터 시작해 1씩 늘려감, 순수 숫자
-	static int numID; // 어떤 ID를 부여할것인가
-	Bank *ownerBank;
-	User *owner;
-	int password; // password는 순수 integer 구성, 카드와 동일
-	int balance; // 잔액
-
-public:
-	
-	Account() { numID = 0; }
-	Account(Bank* bank, User* owner, int pw, int balance);
-	~Account();
-	int getID() { return ID; }
-	bool checkPassward(int); // int 타입의 패스워드를 받아 해당 패스워드가 맞는지 확인
-	void deposit(char, int); // 입금, 입금액 타입(캐시, 수표) 입금액 인풋,
-	void remittance(int, int); //송금 계좌번호, 액수 
-	void widthrawal(int); // 출금
-	void increaseID() { numID++; }
-	int getNumID() { return numID; }
-	int getBalance() { return balance; }
+	int getATMremainCheckNum() { return remainCheckNum; }
 };
 
 // 쓸 수 있으면 map 쓰는게 제일 좋을듯
--- main.cpp
+++ main.cpp
@@ -17,32 +17,40 @@ using namespace std;
 
 int main() {
 
-	int UserStatus = 1; // 세션 시작
+	int UserStatus = 1; // 세션 시작 
 
-	Bank *uriBank = new Bank();
-	Bank *kakaoBank = new Bank("kakao");
+	Bank* uriBank = new Bank();
+	Bank* kakaoBank = new Bank("kakao");
 
-	User *U1 = new User("U1", "최가난");
-	User *U2 = new User("U2", "권부자");
-	User *U3 = new User("U3", "서모녀");
-	User *U4 = new User("U4", "김백규");
-	User *U5 = new User("U5", "버터");
+	User* U1 = new User("U1", "최가난");
+	User* U2 = new User("U2", "권부자");
+	User* U3 = new User("U3", "서모녀");
+	User* U4 = new User("U4", "김백규");
+	User* U5 = new User("U5", "버터");
 
-	ATM *A1 = new ATM(uriBank, "admin", 1357, 100000000, 100, true); // 0
-	ATM *A2 = new ATM(kakaoBank, "master", 2345, 2000000, 0, false); // 1
-	cout << A1->getATMremainCash() << endl;
+	ATM* A1 = new ATM(uriBank, "admin", 1357, 100000000, 100, true); // 0
+	ATM* A2 = new ATM(kakaoBank, "master", 2345, 2000000, 0, false); // 1
+	//cout << A1->getATMremainCash() << endl;
 
-	Account *AC1 = new Account(uriBank, U1, 2345, 10000); // 0
-	Account *AC2 = new Account(uriBank, U2, 3344, 3000000); // 1
-	Account *AC3 = new Account(kakaoBank, U3, 22, 450000); // 2
-	Account *AC4 = new Account(kakaoBank, U1, 1024, 50000); // 3
-	cout << AC1->getBalance() << endl;
-	cout << AC2->getID() << endl;
-	cout << AC3->getID() << endl;
+	Account* AC1 = new Account(uriBank, U1, 2345, 10000); // 0
+	Account* AC2 = new Account(uriBank, U2, 3344, 3000000); // 1
+	Account* AC3 = new Account(kakaoBank, U3, 22, 450000); // 2
+	Account* AC4 = new Account(kakaoBank, U1, 1024, 50000); // 3
+
+	Account* accountlist[100];
+	accountlist[0] = AC1;
+	accountlist[1] = AC2;
+	accountlist[2] = AC3;
+	accountlist[3] = AC4;
+	//cout << AC1->getBalance() << endl;
+	//cout << AC2->getID() << endl;
+	//cout << AC3->getID() << endl;
 
 
+	int currentuser = -1;
 	while (UserStatus != 0) {
 		if (UserStatus == 1) {
+			currentuser = -1;
 			// [의논필요] input을 int 말고 string으로 받아야할까? type error가 안 나도록?
 			// [의논필요] instructor에 atm이 2중언어 지원하는 atm 지원안하는 atm 만들라고 했는데 그럼 atm 선택 단계도 넣어줘야할까?
 			cout << "메뉴를 선택하세요 \n 1. admin panal 보기 \n 2. 개인거래하기 \n 3. exit" << endl;
@@ -70,7 +78,16 @@ int main() {
 			int userAnswer;
 			cin >> userAnswer; // 계좌번호 입력
 			// 본인확인, [의논필요] 본인확인하고 틀리면 어디로 돌아가지?
-			UserStatus = 4;
+
+			// 아직 본인확인을 어떻게 할지는 정해지지 않아서 accountlist의 index를 입력받아 진행하는 식으로 구현하였음.
+			if (userAnswer >= 0 && userAnswer <= 3) {
+				currentuser = userAnswer;
+				UserStatus = 4;
+			}
+			else {
+				cout << "존재하지 않는 계좌입니다. 다시 시도해주십시오." << endl;
+				UserStatus = 3;
+			}
 		}
 		if (UserStatus == 4) { // 거래 종류 선택
 			cout << "어떤 거래를 하시겠습니까? \n 1. 입금 \n 2. 출금 \n 3. 송금 \n 4. 이전 단계로 \n 5. exit" << endl;
@@ -86,12 +103,34 @@ int main() {
 		if (UserStatus == 5) {
 			cout << "입금" << endl;
 			// 입금 함수 실행
-			UserStatus = 4;
+			int type;
+			while (true){
+				cout << "현금으로 입금하시겠습니까? \n 1. 현금 \n 2. 수표" << endl;
+				cin >> type;
+				if (type == 1 or type == 2) { break; }
+				else { cout << "잘못된 입력입니다. 다시시도해 주십시오." << endl; }
+			}
+			cout << "입금하실 금액의 장 당 액수를 입력해주십시오." << endl;
+			int moneytype;
+			cin >> moneytype;
+			cout << "입금하실 금액의 장 수를 입력해주십시오." << endl;
+			int paperNum;
+			cin >> paperNum;
+			bool success = A1->deposit(type, moneytype * paperNum, paperNum, accountlist[currentuser]);
+			if(success){ UserStatus = 4; }
+			else{ UserStatus = 5; }
+			cout << "ATM기 현금 잔액 : " << A1->getATMremainCash() << endl;
+			cout << "ATM기 수표 장 수 : " << A1->getATMremainCheckNum() << endl;
 		}
 		if (UserStatus == 6) {
-			cout << "출금" << endl;
+			cout << "출금하실 금액을 입력해 주십시오." << endl;
+			int money;
+			cin >> money;
 			// 출금 함수 실행
-			UserStatus = 4;
+			bool success = A1->withdrawal(money, accountlist[currentuser]);
+			if (success){ UserStatus = 4; }
+			else { UserStatus = 6; }
+			cout << "ATM기 잔액 : " << A1->getATMremainCash() << endl;
 		}
 		if (UserStatus == 7) {
 			cout << "송금" << endl;
