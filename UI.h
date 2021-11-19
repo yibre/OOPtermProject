433f3442bf2e428e5c4df452e5a718351b7e9e41
shyapple1203 <shyapple1203@naver.com>
2021-11-19 13:19:25 +0900

    Revert "Merge pull request #13 from yibre/HJ1116"
    This reverts commit b9c1fc73bb92de1acb7e576c4cfd17fbb531703f, reversing
    changes made to b77c7e116b8d73a9dfceef49a68c97eef61f0247.

--- UI.h
+++ UI.h
@@ -17,12 +17,8 @@ private:
 		Deposit,
 		Transfer,
 		T_AskTransferType,
-		T_AskToAcc,
+		T_GetToAcc,
 		T_ConfirmToAcc,
-		T_AskAmount_C,
-		T_AskAmount_A,
-		T_Confirm,
-		T_Transfer,
 		Withdrawal,
 		Session1Prompt,
 		Session1Digit,
@@ -37,16 +33,9 @@ private:
 	state = State::GetAccountNum;
 	// int UserStatus = 1;
 	int accountNum = 0;
-	int transferType = 0;
-	int FirstDigit = 0; // needed to be deleted
-	int SecondDigit = 0; // needed to be deleted
-	int accID = -1;
+	int FirstDigit = 0;
+	int SecondDigit = 0;
 	Account* acc = nullptr;
-	int toAccID = -1;
-	Account* toAcc = nullptr; // 송금시에만 사용
-	int fee = 0;
-	int transferAmount = 0;
-	Database* database = nullptr; // database 사용 (바꿔야 할 수도)
 	ATM* atm = nullptr; // the ATM we are using
 	Database* database;
 
@@ -66,13 +55,6 @@ private:
 	State withdrawal();
 	State deposit();
 	State transfer();
-	State t_askTransferType();
-	State t_askToAcc();
-	State t_confirmToAcc();
-	State t_askAmount_c();
-	State t_askAmount_a();
-	State t_confirm();
-	State t_transfer();
 	State Session1Prompt();
 	State session1Digit();
 	State session1Confirm();
@@ -81,5 +63,4 @@ private:
 	State session2Confirm();
 	State session3Prompt();
 	State session3Confirm();
-	void end();
 };
--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@
 using namespace std;
 
 int main() {
-	
 	UI ui;
 	ui.run();
 
